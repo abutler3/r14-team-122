8a9cf63dfaca1e4477082bb89bafe8c4db95d96f
Andrew Butler <abutler3@gmail.com>
2014-10-18 22:22:13 -0500

    Add conditional links and style devise pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get 'pages/home'
 
-  root 'locations#index'
+  root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
