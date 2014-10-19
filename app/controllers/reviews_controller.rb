f0480bea4ce6ae08bb2ce062d721338b3a95c1e8
Andrew Butler <abutler3@gmail.com>
2014-10-19 09:51:35 -0500

    Link reviews to users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,16 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   def index
     @reviews = Review.all
-    respond_with(@reviews)
   end
 
   def show
-    respond_with(@review)
   end
 
   def new
     @review = Review.new
-    respond_with(@review)
   end
 
   def edit
@@ -20,19 +18,40 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @review.save
-    respond_with(@review)
+    @review.user_id = current_user.id
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.json { render :show, status: :created, review: @review }
+      else
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+
   def update
-    @review.update(review_params)
-    respond_with(@review)
+    respond_to do |format|
+      if @review.update(review_params)
+        format.html { redirect_to @review, notice: 'Location was successfully updated.' }
+        format.json { render :show, status: :ok, review: @review }
+      else
+        format.html { render :edit }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
 
   def destroy
     @review.destroy
-    respond_with(@review)
+    respond_to do |format|
+      format.html { redirect_to reviews_url, notice: 'Location was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
+
 
   private
     def set_review
